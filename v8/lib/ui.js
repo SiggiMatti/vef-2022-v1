5cb32546215f3357a8577712ea5ebf9bbf72ce6f
SiggiMatti <sigurdurmatthias@gmail.com>
2022-11-03 18:09:38 +0000

    v8 ready

--- v8/lib/ui.js
+++ v8/lib/ui.js
@@ -27,10 +27,51 @@ export function showScreen(screen) {
  * @returns Elementi fyrir bolla.
  */
 export function createCup(num, svg, onClick) {
-  /*
-  const e = document.createElement('div');
-  e.classList.add("cup")
-  e.innerHTML = 'JavaScript DOM';
-  document.body.appendChild(e);
-  */
+
+
+  const elems = document.getElementsByClassName("cup__button");
+
+  for (var i = 0; i < num; i++){
+    const currentDiv = document.getElementsByClassName("cups")[0];
+
+    // cup div
+    const cup = document.createElement('div');
+    cup.classList.add("cup")
+
+    currentDiv.append(cup);
+
+    // cup__content div
+    const cup__content = document.createElement('div');
+    cup__content.classList.add("cup__content")
+
+    cup.append(cup__content)
+
+    // button div
+    const button = document.createElement("button");
+    button.classList.add("cup__button");
+
+    const buttonNumer = document.createTextNode(i+1);
+    button.appendChild(buttonNumer);
+
+    cup.appendChild(button)
+
+    // svg
+    const mynd = document.createElement("svg");
+    mynd.classList.add("cup__svg");
+    mynd.setAttribute("viewBox", "0 0 32 32");
+    mynd.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+    const svg = document.querySelector('svg').cloneNode(true);
+    cup__content.appendChild(svg.cloneNode(true))
+
+    // path
+    const path = document.createElement("path");
+    path.setAttribute("fill", "currentColor");
+    path.setAttribute("d", "M31 2H1a1 1 0 0 0-1 1v22a3 3 0 0 0 3 3h2v1a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-1h2a3 3 0 0 0 3-3v-3.05c4.72-.52 6-4.68 6-6.95V3a1 1 0 0 0-1-1Zm-7 23a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4h22Zm6-10c0 .19-.06 4.32-4 4.92V4h4Z");
+
+    mynd.appendChild(path);
+
+
+    elems[i].addEventListener('click', onClick);
+  }
 }
